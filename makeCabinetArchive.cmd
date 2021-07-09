c48f25de944451ba59e084fb0dbafd79b0b7cc72
Feldwor <BoQsc@users.noreply.github.com>
2021-07-09 09:22:16 +0300

    Add files via upload

--- makeCabinetArchive.cmd
+++ makeCabinetArchive.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF 
-ECHO In this example, tar folder is being compressed.
+ECHO In this example, "tar" folder is being compressed that exists in %~dp0 (at the same directory as this .cmd file when executed).
 ECHO.
 
 ECHO  Folder to compress: %~dp0tar
@@ -17,9 +17,17 @@ PUSHD "%~dp0CompressedCabinetFiles"
 ECHO Archiving files listed in "%~dp0files.txt" into a .cab file: "%~dp0output\cabinetFile.cab"
 makecab /d "CabinetName1=cabinetFile.cab" /D "DiskDirectoryTemplate=%cd%\output" /f "%~dp0files.txt"
 
-ECHO Testing the extraction of the cabinet file.
-MKDIR "%~dp0CompressedCabinetFiles\extractionTest"
-expand "%~dp0CompressedCabinetFiles\output\cabinetFile.cab" -F:* "%~dp0CompressedCabinetFiles\extractionTest"
+ECHO Create self-extracting .exe executable file.
+COPY /b "%windir%\system32\extrac32.exe"+"%~dp0CompressedCabinetFiles\output\cabinetFile.cab" "%~dp0CompressedCabinetFiles\output\cabinetFile.exe"
+
+ECHO 1. Testing the extraction of the cabinet file.
+MKDIR "%~dp0CompressedCabinetFiles\extractionTestUsingExpand"
+expand "%~dp0CompressedCabinetFiles\output\cabinetFile.cab" -F:* "%~dp0CompressedCabinetFiles\extractionTestUsingExpand"
+
+ECHO 2. Testing the extraction of the cabinet file.
+MKDIR "%~dp0CompressedCabinetFiles\extractionTestUsingExtrac32"
+extrac32 "%~dp0CompressedCabinetFiles\output\cabinetFile.cab" /L "%~dp0CompressedCabinetFiles\extractionTestUsingExtrac32\"
+ECHO Note: seems to not produce even for me in Windows 10
 
 ECHO Leaving the folder.
 POPD
